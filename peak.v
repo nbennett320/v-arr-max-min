8e1816729aa785b4972b2c822e548c03e37627a0
Noah Bennett <nbennett.320@gmail.com>
2021-09-11 00:46:19 -0400

    peak

--- peak.v
+++ peak.v
@@ -1,5 +1,17 @@
 module main
 
+fn find_min(list []int, l int, r int) int {
+	m := (l + r + 1) / 2
+	return match true {
+		(list[m] < list[m+1]) && (list[m] < list[m-1]) { m }
+		(list[m] < list[m+1]) && (m == l) { m }
+		(list[m] < list[m-1]) && (m == r) { m }
+		(list[m] <= list[m-1]) { find_min(list, m+1, r) }
+		(list[m] <= list[m+1]) { find_min(list, l, m-1) }
+		else { -1 }
+	}
+}
+
 fn find_max(list []int, l int, r int) int {
 	m := (l + r + 1) / 2
 	return match true {
@@ -17,6 +29,8 @@ fn main() {
 
 	println(list)
 
-	p := find_max(list, 0, list.cap-1)
-	println('max index: $p')
+	imax := find_max(list, 0, list.cap-1)
+	imin := find_min(list, 0, list.cap-1)
+	println('max index: $imax')
+	println('min index: $imin')
 }
